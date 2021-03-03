d15340eee8485b22ac8a5a2758778a1d6393cf32
OkitaSan <tssjy999@163.com>
2021-03-04 05:46:06 +0800

    split by line with \n added to the end of the character was finished

--- user/xargs.c
+++ user/xargs.c
@@ -6,20 +6,25 @@
 /// @param input the input string
 /// @param start_idx the starting index of the input
 /// @returns the index of the first character of the next line,and -1 if it meets the end of the character
-uint split_by_line(char* dst,const char* input,uint start_idx){
+int split_by_line(char* dst,const char* input,uint start_idx){
     // the sequence is a string which gets to the first character of the line
     const char* seq = input + start_idx;
+    uint len = 0;
     // if the input is neither 0 or EOF,then we go into the loop
     while(*seq != '\0' && *seq != '\n'){
         *dst++ = *seq++;
         start_idx++;
+        len++;
     }
-    
+    if(len != 0 && *seq == '\n'){
+        dst[0] = '\n';
+        start_idx++;
+    }
     return *seq == '\0' ? -1 : start_idx;
 }
 
-int main(){
-    char buffer[100] = {1};
+int split_by_line_test(){
+    char buffer[100];memset(buffer,0,100);
     char * test = "The\n quick\n brown\n fox\n jumps\n over\n the\n lazy dog\n";
     uint idx = 0;
     while ((idx = split_by_line(buffer,test,idx)) != -1)
