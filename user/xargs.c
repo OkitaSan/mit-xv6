c9af5d45dfcfee6c8b94349787717dcd73c706a8
OkitaSan <tssjy999@163.com>
2021-03-05 18:25:04 +0800

    my final solution for util

--- user/xargs.c
+++ user/xargs.c
@@ -1,5 +1,6 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/param.h"
 #include "user/user.h"
 /// split a string by line
 /// @param dst the destination for splitee
@@ -17,12 +18,53 @@ int split_by_line(char* dst,const char* input,uint start_idx){
         len++;
     }
     if(len != 0 && *seq == '\n'){
-        dst[0] = '\n';
+        //dst[0] = '\n';
         start_idx++;
     }
     return *seq == '\0' ? -1 : start_idx;
 }
+/**
+ * function xargs
+ * @param programm command xargs nees to execute
+ * @param input the argument comes along with the programm
+ * 
+ * 
+ * 
+ * 
+ */
+void xargs(char* programm,char* input){
+    // the buffer stores the input from stdin
+    // lines stands for the command got from each line
 
+    char buffer[512],lines[20];
+    // the index 
+    uint idx = 0;
+    read(0,buffer,200);
+    //TODELETE
+    //printf("%s\n",buffer);
+    while((idx = split_by_line(lines,buffer,idx)) != -1){
+        // printf("%s",buffer);
+        // create chile process
+        if(fork() == 0){
+            //printf("%s %s",programm,input);
+            char* arguments[MAXARG] = {programm,input,lines};
+            // TODELETE
+            // printf("index:%d,programm:%s,input:%s,lines:%s\n",idx,programm,input,lines); 
+            exec(programm,arguments);
+        }
+        wait((int*)0);
+        memset(lines,0,20);
+    }
+    return;
+}
+int main(int argc, char **argv){
+    if(argc < 3){
+        printf("Usage: xargs <command> <argument>");
+        exit(0);
+    }
+    xargs(argv[1],argv[2]);
+    exit(0);
+}
 int split_by_line_test(){
     char buffer[100];memset(buffer,0,100);
     char * test = "The\n quick\n brown\n fox\n jumps\n over\n the\n lazy dog\n";
